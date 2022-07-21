b3c9930e17eb16836d936a6e22e562d9f4f9642a
Krishna <gopalakrishnan567890@gmail.com>
2022-07-21 19:13:51 +0530

    Assisted prgm commited from local repo

---
+++ ThreadSync/ThreadSync.java
@@ -0,0 +1,62 @@
+package ThreadSync;
+
+
+import java.io.*; 
+import java.util.*; 
+class Sender 
+{ 
+    public void send(String msg) 
+    { 
+        System.out.println("Sending\t"  + msg ); 
+        try
+        { 
+            Thread.sleep(1000); 
+        } 
+        catch (Exception e) 
+        { 
+            System.out.println("Thread  interrupted."); 
+        } 
+        System.out.println("\n" + msg + "Sent"); 
+    } 
+} 
+class ThreadedSend extends Thread 
+{ 
+    private String msg; 
+    private Thread t; 
+    Sender  sender; 
+    ThreadedSend(String m,  Sender obj) 
+    { 
+        msg = m; 
+        sender = obj; 
+    } 
+  
+    public void run() 
+    {  
+        synchronized(sender) 
+        { 
+            sender.send(msg); 
+        } 
+    } 
+} 
+class ThreadSync 
+{ 
+    public static void main(String args[]) 
+    { 
+        Sender snd = new Sender(); 
+        ThreadedSend S1 = 
+            new ThreadedSend( " Good Morning " , snd ); 
+        ThreadedSend S2 = 
+            new ThreadedSend( " Be Positive " , snd ); 
+        S1.start(); 
+        S2.start(); 
+        try
+        { 
+            S1.join(); 
+            S2.join(); 
+        } 
+        catch(Exception e) 
+        { 
+            System.out.println("Interrupted"); 
+        } 
+    } 
+} 
