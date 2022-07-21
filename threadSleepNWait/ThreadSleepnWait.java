a2018c97fc2ec0c6bcc21cc008e4555024c159c4
Krishna <gopalakrishnan567890@gmail.com>
2022-07-21 19:02:14 +0530

    Assisted prgm from local repo

---
+++ threadSleepNWait/ThreadSleepnWait.java
@@ -0,0 +1,76 @@
+package threadSleepNWait;
+
+
+class Chat {
+	boolean flag = false;
+	public synchronized void Question(String msg) {
+        if (flag==true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+      }
+        System.out.println(msg);
+        flag = true;
+        notify();// a method from object class which provide signal to next thread
+    }
+	
+	public synchronized void Answer(String msg) {
+        if (flag==false) {
+            try {
+                wait(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(msg);
+        flag = false;
+        notify();
+    }
+}
+
+class Teacher implements Runnable {
+    Chat m;
+    String[] s1 = { "Hi", "How are you ?", "I am also doing fine!" };
+
+    public Teacher(Chat m1) {
+        this.m = m1;
+        new Thread(this, "Teacher").start();
+    }
+
+    public void run() {
+        for (int i = 0; i < s1.length; i++) {
+            m.Question(s1[i]);
+        }
+    }
+}
+
+class Student implements Runnable {
+    Chat m;
+    String[] s2 = { "Hi", "I am good, what about you?", "Great!" };
+
+    public Student(Chat m2) {
+        this.m = m2;
+        new Thread(this, "Student").start();
+        /*
+         * Thread t=new Thread(this,"Student"); 
+         * t.start();
+         */
+    }
+
+    public void run() {
+        for (int i = 0; i < s2.length; i++) {
+            m.Answer(s2[i]);
+        }
+    }
+}
+public class ThreadSleepnWait {
+    public static void main(String[] args) {
+        Chat m = new Chat();
+        new Teacher(m);
+        new Student(m);
+    }
+}
+
