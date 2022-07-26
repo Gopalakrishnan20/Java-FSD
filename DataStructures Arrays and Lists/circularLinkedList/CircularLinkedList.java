93213d2d7a187f902e1ebd6fb1ef6045a9f8fba3
Krishna <gopalakrishnan567890@gmail.com>
2022-07-26 17:23:51 +0530

    Assisted prgm commited from local repo

---
+++ DataStructures Arrays and Lists/circularLinkedList/CircularLinkedList.java
@@ -0,0 +1,63 @@
+package circularLinkedList;
+
+public class CircularLinkedList {
+	static class Node {
+		int data;
+		Node next;
+
+		Node(int d) {
+			data = d;
+			next = null;
+		}
+	}
+
+	Node head;
+
+CircularLinkedList()   
+{ 
+head = null; 
+}
+
+	void sortedInsert(Node new_node) {
+		Node current = head;
+		if (current == null) {
+			new_node.next = new_node;
+			head = new_node;
+		} else if (current.data >= new_node.data) {
+			while (current.next != head)
+				current = current.next;
+			current.next = new_node;
+			new_node.next = head;
+			head = new_node;
+		} else {
+			while (current.next != head && current.next.data < new_node.data)
+				current = current.next;
+			new_node.next = current.next;
+			current.next = new_node;
+		}
+	}
+
+	void printList() {
+		if (head != null) {
+			Node temp = head;
+			do {
+				System.out.print(temp.data + " ");
+				temp = temp.next;
+			} while (temp != head);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CircularLinkedList list = new CircularLinkedList();
+		int arr[] = new int[] { 12, 56, 2, 11, 1, 90 };
+		Node temp = null;
+		for (int i = 0; i < 6; i++) {
+			temp = new Node(arr[i]);
+			list.sortedInsert(temp);
+		}
+		list.printList();
+
+	}
+
+}
