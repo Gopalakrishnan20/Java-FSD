8b3858fc903838d1b4cda7028c229f705e2e24bb
Krishna <gopalakrishnan567890@gmail.com>
2022-08-01 18:32:10 +0530

    Uploaded ServletConcepts

---
+++ Developing Dynamic Web Pages using Servlets/ServletConcept/src/main/java/servlet/NameServlet.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/NameServlet")
+public class NameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public NameServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+        String fname = request.getParameter("fname");
+        String lname = request.getParameter("lname");
+        
+        PrintWriter out = response.getWriter();
+    out.println("<html><body>");
+    out.println("Your full name is " + fname + " " + lname);
+    out.println("</body></html>");
+
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
