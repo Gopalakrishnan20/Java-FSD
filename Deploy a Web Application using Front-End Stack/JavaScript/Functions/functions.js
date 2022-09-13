b1eb9d5ad966ee708adb7aac5871890268ddaaa1
Krishna <gopalakrishnan567890@gmail.com>
2022-09-13 15:16:35 +0530

    Js Functions

---
+++ Deploy a Web Application using Front-End Stack/JavaScript/Functions/functions.js
@@ -0,0 +1,19 @@
+var x = (2 * 3) + 5;
+	var y = 3 * 4;
+	
+	var result = myFunction(2,3);
+	console.log(result);
+	
+	function myFunction(num1, num2) {
+		var a = num1 * num2;
+		var b = num1 + num2;
+		return(a + b);
+	}
+	
+	console.log( myFunction(3, 4));
+	
+	function toCelcius(f){
+			return (5/9) * (f-32);
+	}
+	
+	console.log("The temperature is "+ toCelcius(60));
